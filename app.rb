ac3cc9643280878a120b2fe0ec6f24f7d0974967
Randi Schreiner <mimirandi21@gmail.com>
2020-11-12 09:01:39 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    configure do
+        enable :sessions
+    
+    end
+
+    get '/' do
+        erb :index
+    end
+
+    post '/checkout' do
+        @sessions = session
+        item = params["item"]
+        @sessions[:item] = item
+    end
 end
